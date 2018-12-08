3360b462c64df760eb91e6f75ec34f468b3c4ad0
richku <45708126+richku@users.noreply.github.com>
2018-12-08 17:03:42 +0900

    Ch02
    Ch02

---
+++ Ch02/Lab01.py
@@ -0,0 +1,28 @@
+size = input("집의 크기는 얼마로 할까요?")
+size = int(size)
+import turtle
+t = turtle.Turtle()
+t.shape("turtle")
+
+t.forward(size)
+t.right(90)
+t.forward(size)
+t.right(90)
+t.forward(size)
+t.right(90)
+t.forward(size)
+t.right(90)
+
+t.forward(size)
+t.left(120)
+t.forward(size)
+t.left(120)
+t.forward(size)
+t.left(120)
+
+
+
+
+
+
+
---
+++ Ch02/Vari05.py
@@ -0,0 +1,11 @@
+name = input("이름을 입력하세요")
+print(name,"씨 안녕하세요?")
+print("파이썬에 오신을 것을 환영합니다")
+
+first = input("첫번째 정수를 입력하세요")
+second = input("두번째 정수를 입력하세요")
+first = int(first)
+second = int(second)
+sum_all = first + second
+
+print(first,"과", second,"의 합은", sum_all,"입니다.")
