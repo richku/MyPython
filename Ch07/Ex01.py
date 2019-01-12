5959f942c9c863116a64428473529e73fb9884bb
richku <45708126+richku@users.noreply.github.com>
2019-01-12 17:05:35 +0900

    Ch07
    Ch07

---
+++ Ch07/Ex01.py
@@ -0,0 +1,8 @@
+def fun(x):
+    print("함수가 호출되었습니다.")
+    return x +2
+
+a = 5
+b = fun(a)
+print(a)
+print(b)
---
+++ Ch07/Ex02.py
@@ -0,0 +1,10 @@
+def print_address():
+    print("주소록을 호출하셨습니다.")
+    print("감사합니다.")
+    
+
+
+print("컴퓨터 시작")
+print_address()
+print("컴퓨터 종료")
+
